b0a999a0cefe1ac840216ec2fa2ff490a630db5e
dkdhWkwmdsk <149846616+dkdhWkwmdsk@users.noreply.github.com>
2023-11-04 10:40:03 +0900

    Update ㅎㅎㅎ.py

--- ㅎㅎㅎ.py
+++ ㅎㅎㅎ.py
@@ -1,4 +1,13 @@
 import  streamlit as st
-
+import pandas as pd
+import numpy as np
 st.title("하하하하핳ㅎ하")
 st.write("HELLO **world**!")
+dataframe=pd.DataFrame({
+  'first column':[1,2,3,4],
+  'second column':[10,20,30,40],
+})
+
+st.dataframe(dataframe, use_container_width=False)
+
+st.table(dataframe)
