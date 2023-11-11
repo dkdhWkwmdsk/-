2be3f271ff4ce29759c312f119bdfb1615252f22
dkdhWkwmdsk <149846616+dkdhWkwmdsk@users.noreply.github.com>
2023-11-11 09:47:39 +0900

    Update ㅎㅎㅎ.py

--- ㅎㅎㅎ.py
+++ ㅎㅎㅎ.py
@@ -17,3 +17,11 @@ col1, col2, col3 = st.columns(3)
 col1.metric(label="달러USD",value="1,228원",delta="-2.00원")
 col2.metric(label="일본JPY(100엔",value="958.63원",delta="-7.44원")
 col3.metric(label="유럽연랍EUR",value+"1,335.82원",delta="11.44원")
+
+tab1, tab2 = st.tabs(["Cat","Dog"])
+with tab1:
+  st.header("A cat")
+  st.image("https://startic.streamlit.fo/examples/cat.jpg",width=200)
+  with tab2:
+  st.header("A dog")
+  st.image("https://startic.streamlit.fo/examples/dog.jpg",width=200)
